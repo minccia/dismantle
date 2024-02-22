03de72982d5d40bd91dc4bb92ea67cfd4bab16c8
minccia <lusyva.pro@gmail.com>
2024-02-22 19:29:29 -0300

    Fix identation to use 4 spaces

--- lib/hello_world/hello_world.asm
+++ lib/hello_world/hello_world.asm
@@ -1,17 +1,17 @@
 section .data
-        message DB 'Hello, world!', 0xA
-        message_len EQU $ - message
+    message DB 'Hello, world!', 0xA
+    message_len EQU $ - message
 
 section .text 
 global _start
 
 _start:
-        MOV eax, 4
-        MOV ebx, 1
-        MOV ecx, message
-        MOV edx, message_len
-        INT 80h
+    MOV eax, 4
+    MOV ebx, 1
+    MOV ecx, message
+    MOV edx, message_len
+    INT 80h
 
-        MOV eax, 1
-        MOV ebx, 0
-        INT 80h
+    MOV eax, 1
+    MOV ebx, 0
+    INT 80h
