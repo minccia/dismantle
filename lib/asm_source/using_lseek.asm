234b0e75fcd43a5929e4debe8d77dbf4e0448b5a
minccia <lusyva.pro@gmail.com>
2024-03-07 18:02:53 -0300

    Fix lseek error handling

--- lib/asm_source/using_lseek.asm
+++ lib/asm_source/using_lseek.asm
@@ -1,30 +1,49 @@
 section .data
-    pathname: DD "/home/paola/test.txt"
+    pathname DB "../support/test.txt", 0
+    open_failure_msg DB "File does not exist", 0xA
+    open_failure_msg_len EQU $ - open_failure_msg
 
 section .bss
-    buffer: resb 10
+    buffer resb 10
 
 section .text
 
 global _start
 
 _start:
-    MOV eax, 5          ;  sys_open
-    MOV ebx, pathname   ;
-    mov ecx, 0          ;
-    INT 80h             ;
+    MOV eax, 5            ; sys_open
+    MOV ebx, pathname     ;
+    mov ecx, 0            ;
+    INT 80h               ;
 
-    MOV ebx, eax        ; sys_open file descriptor
-    MOV eax, 19         ; sys_lseek
-    MOV ecx, 20         ; offset
-    mov edx, 0          ; whence SEEK_SET
+    CMP eax, 0            ;
+    JNS open_success
+
+open_failure:
+    MOV eax, 4            ; sys_write
+    MOV ebx, 1            ; stdout
+    MOV ecx, open_failure_msg
+    MOV edx, open_failure_msg_len
     INT 80h
 
-    MOV eax, 3          ; sys_read
-    MOV ecx, buffer     ;
-    MOV edx, 10         ; read 10 bytes
+    MOV ecx, 1            ; status code
+    JMP end
+
+open_success:
+    MOV ebx, eax          ; sys_open file descriptor
+    MOV eax, 19           ; sys_lseek
+    MOV ecx, 20           ; offset
+    mov edx, 0            ; whence SEEK_SET
     INT 80h
 
-    MOV eax, 1          ; sys_exit
-    XOR ebx, ebx        ;
+    MOV eax, 3            ; sys_read
+    MOV ecx, buffer       ;
+    MOV edx, 10           ; read 10 bytes
+    INT 80h
+
+    MOV ecx, 0
+
+end:
+    MOV eax, 1            ; sys_exit
+    MOV ebx, ecx          ;
     INT 80h
